ef3285436ee156eac0c54dc8f36cdb6892370ad1
niloocampos <nvancity@gmail.com>
2024-03-06 14:02:15 -0800

    changed className of layout file

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <div className="container mx-auto px-12">
+      {children}
+      </div>
+      </body>
     </html>
   );
 }
