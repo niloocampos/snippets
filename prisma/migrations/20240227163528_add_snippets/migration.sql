9d8fd078bec85cdfa08c72815e4a2350d2b4a827
niloocampos <nvancity@gmail.com>
2024-03-06 14:15:07 -0800

    Added Migration.sql and schema.prisma

---
+++ prisma/migrations/20240227163528_add_snippets/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Snippet" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "title" TEXT NOT NULL,
+    "code" TEXT NOT NULL
+);
