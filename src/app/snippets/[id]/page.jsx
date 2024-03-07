75e894ab4a6aac356b5a1d744e093d3853266502
niloocampos <nvancity@gmail.com>
2024-03-07 11:52:24 -0800

    Implemented generateStaticParams function to allow some caching to occur for a dynamic page

--- src/app/snippets/[id]/page.jsx
+++ src/app/snippets/[id]/page.jsx
@@ -39,3 +39,14 @@ export default async function SnippetShowPage(props) {
   );
 };
 
+export async function generateStaticParams() {
+  const snippets = await db.snippet.findMany();
+
+  return snippets.map((snippet) => {
+    return {
+      id: snippet.id.toString(),
+    };
+  });
+}
+
+
